55d4b1cece847a71f6d978c878a99a5e8d4e763a
Alexandra Bashkirova <s.bshkrva@gmail.com>
2020-03-12 20:24:44 +0700

    Add first test - T1.1

--- test.sh
+++ test.sh
@@ -1,1 +1,23 @@
 #!/usr/bin/env bash
+
+function testcase {
+    ACT=$1;
+    EXPECTED_CODE=$2;
+    PURPOSE=$3
+
+    echo -e "$PURPOSE"
+    echo "java -jar app.jar $ACT"
+
+    ./run.sh $ACT
+    RES=$?
+    if [ $RES -eq $EXPECTED_CODE ]; then
+        echo -e "Test passed"
+    else
+        echo -e "Test failed. Expected $EXPECTED_CODE. Actual $RES"
+    fi
+    echo
+}
+
+# Справка
+#T1.1
+testcase "" 1 "#T1.1: R1.8 Печать справки"
