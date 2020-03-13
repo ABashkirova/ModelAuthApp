2522ec73acc229d537642b8777b45b7d7b2e3e66
ABashkirova <alex.bashkirova@gmail.com>
2020-03-14 02:52:14 +0700

    Fix format file

--- src/service/AuthenticationService.kt
+++ src/service/AuthenticationService.kt
@@ -9,10 +9,22 @@ class AuthenticationService {
     }
 
     private val users = listOf(
-        User(login = "sasha", hash = "834d80053ffca546c11b486db79dbe31878b67e7cc9f6f74462f2347f28c767b"), // password = "123"
-        User(login = "admin", hash = "affb3cd6b447ff1630c9ebf933f9abfad257bc32a0e5a25c80341bbc8e3d3a9b"), // password = "qwerty",
-        User(login = "q", hash = "cd18eba9806a168bb471da898670708d172ff3ddb60a3ad697c4ee0c51f95986"), //  password = "@#\$%^&*!",
-        User(login = "aleksandra", hash = "91fdccfd2cdf63a743242d8f6a315a77dc498adf13db05e5e875d147a0f47cf2") // password = "abc",
+        User(
+            login = "sasha",
+            hash = "834d80053ffca546c11b486db79dbe31878b67e7cc9f6f74462f2347f28c767b"
+        ), // password = "123"
+        User(
+            login = "admin",
+            hash = "affb3cd6b447ff1630c9ebf933f9abfad257bc32a0e5a25c80341bbc8e3d3a9b"
+        ), // password = "qwerty",
+        User(
+            login = "q",
+            hash = "cd18eba9806a168bb471da898670708d172ff3ddb60a3ad697c4ee0c51f95986"
+        ), //  password = "@#\$%^&*!",
+        User(
+            login = "aleksandra",
+            hash = "91fdccfd2cdf63a743242d8f6a315a77dc498adf13db05e5e875d147a0f47cf2"
+        ) // password = "abc",
     )
 
     fun validateLogin(login: String): Boolean {
