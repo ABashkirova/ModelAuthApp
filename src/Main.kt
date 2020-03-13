ed6788313af97ee8bc3b006a9f7ddda9b5b647da
Alexandra Bashkirova <s.bshkrva@gmail.com>
2020-03-13 22:26:13 +0700

    Add fun printHelp - s2i3

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,29 @@
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
-    println("Hello world")
+    printHelp()
+    exitProcess(status = 1)
 }
 
 fun argsAreNotEmpty(args: Array<String>): Boolean = args.isNotEmpty()
 
+fun printHelp() {
+    val helpMessage = """
+        Возможные аргументы программы:
+        -h                              - Вызов справки
+        
+        Аутентифицироваться 
+        -login  <string>                - Логин пользователя, строка, строчные буквы. Не более 10 символов
+        -pass   <string>                - Пароль пользователя
+        
+        Авторизоваться
+        -res    <PATH.TO.RESOURCE>      - Абсолютный путь до запрашиваемого ресурса
+        -role   <READ|WRITE|EXECUTE>    - Уровень доступа к ресурсу
+        
+        Активность
+        -ds     <yyyy-mm-dd>            - Дата начала сессии с ресурсом
+        -de     <yyyy-mm-dd>            - Дата окончания сессии с ресурсом, формат
+        -vol    <int>	                - Потребляемый объем, целое число
+    """.trimIndent()
+    println(helpMessage)
+}
