d725a5e326aba3fe5c6eda210a169cd64d144a91
ABashkirova <alex.bashkirova@gmail.com>
2020-03-14 01:41:19 +0700

    Move to ArgHandler: fun helpIsNeeded and fun authenticationIsNeeded - s3i9

--- src/controller/ArgHandler.kt
+++ src/controller/ArgHandler.kt
@@ -18,6 +18,22 @@ class ArgHandler(private val args: Array<String>) {
         } else null
     }
 
+    fun authenticationIsNeeded(): Boolean = when {
+        argsAreNotEmpty() && args.size >= 4 -> {
+            argsContainsFlag(ArgKey.LOGIN.value)
+                    && argsContainsFlag(ArgKey.PASSWORD.value)
+        }
+        else -> false
+    }
+
+    fun helpIsNeeded(): Boolean {
+        return if (argsAreNotEmpty()) {
+            args.contains(ArgKey.HELP.value)
+        } else true
+    }
+
+    private fun argsAreNotEmpty(): Boolean = args.isNotEmpty()
+
     private fun argsContainsFlag(flag: String): Boolean =
         args.contains(flag) && (args.size > args.indexOf(flag) + 1)
 }
