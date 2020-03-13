a4a66b93ab291782c7b3c130d20a01d0f9b9e604
ABashkirova <alex.bashkirova@gmail.com>
2020-03-14 02:02:15 +0700

    Add class AuthenticationService - s3i11

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-kotlinc ./src/controller/ArgHandler.kt ./src/model/User.kt ./src/Main.kt -include-runtime -d app.jar
+kotlinc ./src/service/AuthenticationService.kt ./src/controller/ArgHandler.kt ./src/model/User.kt ./src/Main.kt -include-runtime -d app.jar
