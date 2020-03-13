bca053aed985a5d74e0870a03a6a1470b2fc4c78
ABashkirova <alex.bashkirova@gmail.com>
2020-03-14 02:03:23 +0700

    Move to AuthenticationService user list - s3i11.1

--- src/service/AuthenticationService.kt
+++ src/service/AuthenticationService.kt
@@ -1,5 +1,13 @@
 package service
 
+import model.User
+
 class AuthenticationService {
+    private val users = listOf(
+        User(login = "sasha", password = "123"),
+        User(login = "admin", password = "qwerty"),
+        User(login = "q", password = "@#\$%^&*!"),
+        User(login = "aleksandra", password = "abc")
+    )
 
 }
