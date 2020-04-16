67fde4559c70082a61d1efcb260da7cac4d33ae0
ABashkirova <alex.bashkirova@gmail.com>
2020-04-16 23:54:43 +0700

    Delete scripts

--- scripts/BUILD.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ./build
-
-./gradlew build
-mv ./build/libs/*-fat-*.jar ./AAA.jar
--- scripts/RUN.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-java -Dlog4j.configurationFile=src/main/resources/log4j2.xml \
-    -classpath "src/main/kotlin:src:src/main/resources:AAA.jar" \
-    xyz.sashenka.modelauthapp.Main "$@"
