b81c035c3d0a2dd1132a678baa1e1c08caf68a05
Max <kozlyakov.m@gmail.com>
2020-04-24 20:51:57 +0700

    Add UserServlet, AuthorityServlet, ActivityServlet

--- web/src/main/kotlin/MyGuiceServletConfig.kt
+++ web/src/main/kotlin/MyGuiceServletConfig.kt
@@ -2,8 +2,10 @@ import com.google.inject.Guice
 import com.google.inject.Injector
 import com.google.inject.servlet.GuiceServletContextListener
 import com.google.inject.servlet.ServletModule
-
+import servlets.ActivityServlet
 import servlets.EchoServlet
+import servlets.AuthorityServlet
+import servlets.UserServlet
 
 
 class MyGuiceServletConfig : GuiceServletContextListener() {
@@ -12,6 +14,9 @@ class MyGuiceServletConfig : GuiceServletContextListener() {
             return Guice.createInjector(object: ServletModule() {
                 override fun configureServlets() {
                     serve("/echo/*").with(EchoServlet::class.java)
+                    serve("/ajax/user").with(UserServlet::class.java)
+                    serve("/ajax/authority").with(AuthorityServlet::class.java)
+                    serve("/ajax/activity").with(ActivityServlet::class.java)
                 }
             })
     }
--- web/src/main/kotlin/servlets/EchoServlet.kt
+++ web/src/main/kotlin/servlets/EchoServlet.kt
@@ -4,7 +4,6 @@ import com.google.inject.Singleton
 import org.apache.logging.log4j.kotlin.loggerOf
 import java.io.IOException
 import javax.servlet.ServletException
-import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
