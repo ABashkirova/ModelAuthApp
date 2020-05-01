280af40e01974ac326902142e8c406b16cadb2ee
Alexandra Bashkirova <s.bshkrva@gmail.com>
2020-05-01 15:25:57 +0700

    Add inject gson to servlets

--- web/src/main/kotlin/xyz/sashenka/webapplication/di/GsonProvider.kt
+++ web/src/main/kotlin/xyz/sashenka/webapplication/di/GsonProvider.kt
@@ -1,10 +1,11 @@
 package xyz.sashenka.webapplication.di
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import javax.inject.Provider
 
 class GsonProvider: Provider<Gson> {
     override fun get(): Gson {
-        return Gson()
+        return GsonBuilder().setPrettyPrinting().create()
     }
 }
--- web/src/main/kotlin/xyz/sashenka/webapplication/servlets/ActivityServlet.kt
+++ web/src/main/kotlin/xyz/sashenka/webapplication/servlets/ActivityServlet.kt
@@ -1,5 +1,7 @@
 package xyz.sashenka.webapplication.servlets
 
+import com.google.gson.Gson
+import com.google.inject.Inject
 import com.google.inject.Singleton
 import java.io.IOException
 import javax.servlet.ServletException
@@ -9,8 +11,12 @@ import javax.servlet.http.HttpServletResponse
 
 @Singleton
 class ActivityServlet : HttpServlet() {
+    @Inject
+    lateinit var gson: Gson
+
     @Throws(ServletException::class, IOException::class)
     override fun service(request: HttpServletRequest, response: HttpServletResponse) {
-        response.writer.write("ActivityServlet. Method: ${request.method}")
+        val json = gson.toJson("ActivityServlet. Method: ${request.method}".split(" "))
+        response.writer.write(json)
     }
 }
