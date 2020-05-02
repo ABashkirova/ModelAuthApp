8d6c92bc9f99dfdbbef0022199e1fee50824abb6
Max <kozlyakov.m@gmail.com>
2020-05-02 17:53:56 +0700

    Mark UserSession and UserResources fields with Expose annotation

--- app/src/main/kotlin/xyz/sashenka/modelauthapp/model/domain/UserSession.kt
+++ app/src/main/kotlin/xyz/sashenka/modelauthapp/model/domain/UserSession.kt
@@ -1,9 +1,16 @@
 package xyz.sashenka.modelauthapp.model.domain
 
+import com.google.gson.annotations.Expose
+
 data class UserSession(
+    @Expose
     val user: String,
+    @Expose
     val resource: String,
+    @Expose
     val dateStart: String,
+    @Expose
     val dateEnd: String,
+    @Expose
     val volume: Int
 )
