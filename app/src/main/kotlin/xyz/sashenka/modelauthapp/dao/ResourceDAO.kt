8c03317a02b53481784600145a1f18067b50c7ab
Max <kozlyakov.m@gmail.com>
2020-05-02 21:10:17 +0700

    Create methods requestUserResourceByUserId and requestUserResourceById

--- app/src/main/kotlin/xyz/sashenka/modelauthapp/dao/ResourceDAO.kt
+++ app/src/main/kotlin/xyz/sashenka/modelauthapp/dao/ResourceDAO.kt
@@ -18,6 +18,10 @@ class ResourceDAO(private val dbConnection: Connection) {
                 """
     private val selectUserResourceByIdSql: String
         get() = "SELECT * FROM ACCESS WHERE ID = ?"
+    private val selectUserResourceByUserIdSql: String
+        get() = "SELECT * FROM ACCESS WHERE ID = ?"
+    private val selectAllUserResourcesSql: String
+        get() = "SELECT * FROM ACCESS"
 
     fun requestAccessByResource(login: String, resource: String, role: String): DBAccess? {
         val statement = dbConnection.prepareStatement(selectUserResourcesByLoginSql)
@@ -41,7 +45,7 @@ class ResourceDAO(private val dbConnection: Connection) {
         val result: MutableList<DBAccess> = mutableListOf()
         val statement = dbConnection.createStatement()
         statement.use {
-            val resultSet = it.executeQuery("SELECT * FROM ACCESS")
+            val resultSet = it.executeQuery(selectAllUserResourcesSql)
             while (resultSet.next()) {
                 result.add(
                     DBAccess(
@@ -54,6 +58,43 @@ class ResourceDAO(private val dbConnection: Connection) {
             }
             return result.toList()
         }
+    }
+    fun requestAccessByUserId(userId: Int): List<DBAccess> {
+        val result: MutableList<DBAccess> = mutableListOf()
+        val statement = dbConnection.prepareStatement(selectUserResourceByUserIdSql)
+        statement.use {
+            it.setValues(userId)
+            val resultSet = it.executeQuery()
+            while (resultSet.next()) {
+                result.add(
+                    DBAccess(
+                        id = resultSet.getInt("ID"),
+                        userId = resultSet.getInt("USER_ID"),
+                        resource = resultSet.getString("RESOURCE"),
+                        role = resultSet.getString("ROLE")
+                    )
+                )
+            }
+            return result.toList()
+        }
+    }
+
+    fun requestAccessById(id: Int): DBAccess? {
+        var result: DBAccess? = null
+        val statement = dbConnection.prepareStatement(selectUserResourceByIdSql)
+        statement.use {
+            it.setValues(id)
+            val resultSet = it.executeQuery()
+            if (resultSet.next()) {
+                result = DBAccess(
+                    id = resultSet.getInt("ID"),
+                    userId = resultSet.getInt("USER_ID"),
+                    resource = resultSet.getString("RESOURCE"),
+                    role = resultSet.getString("ROLE")
+                )
+            }
+        }
+        return result
     }
 
 }
