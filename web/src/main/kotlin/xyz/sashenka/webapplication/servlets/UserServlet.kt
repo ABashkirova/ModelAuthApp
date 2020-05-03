4fd2da84e6084884b75228b5fa385853ae5de394
ABashkirova <alex.bashkirova@gmail.com>
2020-05-03 14:00:55 +0700

    Refactor User Servlet

--- web/src/main/kotlin/xyz/sashenka/webapplication/servlets/UserServlet.kt
+++ web/src/main/kotlin/xyz/sashenka/webapplication/servlets/UserServlet.kt
@@ -5,7 +5,9 @@ import com.google.inject.Inject
 import com.google.inject.Singleton
 import org.apache.logging.log4j.kotlin.KotlinLogger
 import xyz.sashenka.modelauthapp.dao.UserDAO
+import xyz.sashenka.modelauthapp.model.domain.User
 import xyz.sashenka.webapplication.di.logger.InjectLogger
+import xyz.sashenka.webapplication.isInteger
 import java.io.IOException
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServlet
@@ -16,29 +18,60 @@ import javax.servlet.http.HttpServletResponse
 class UserServlet : HttpServlet() {
     @Inject
     lateinit var gson: Gson
+
     @Inject
     lateinit var userDAO: UserDAO
+
     @InjectLogger
     lateinit var logger: KotlinLogger
 
     @Throws(ServletException::class, IOException::class)
     override fun service(request: HttpServletRequest, response: HttpServletResponse) {
-        val json: String
         if (request.queryString == null) {
-            json = gson.toJson(userDAO.requestAllUsers())
-            response.writer.write(json)
+            response.writer.write(allUsersToJson())
         } else {
-            val id = request.getParameter("id")
-            if (id != null) {
-                try {
-                    json = gson.toJson(userDAO.requestUserById(id.toInt()))
-                    response.writer.write(json)
-                } catch (e: NumberFormatException) {
-                    response.sendError(400, e.message)
-                }
-            } else {
-                response.sendError(404)
-            }
+            handleRequestWithUserId(request, response)
         }
+    }
+
+    fun handleRequestWithUserId(request: HttpServletRequest, response: HttpServletResponse) {
+        val idParameter = request.getParameter("id")
+        if (idParameter.isNullOrEmpty()) {
+            sendErrorParameterIsEmpty(response)
+        } else if (!idParameter.isInteger()) {
+            sendErrorParameterIsNotInteger(response)
+        } else {
+            val id = idParameter.toInt()
+            writeUserResponse(id, response)
+        }
+    }
+
+    private fun writeUserResponse(userId: Int, response: HttpServletResponse) {
+        val user = userDAO.requestUserById(userId)
+        if (user == null) {
+            sendErrorNotFoundUserWithId(response)
+        } else {
+            response.writer.write(useToJson(user))
+        }
+    }
+
+    private fun sendErrorParameterIsEmpty(response: HttpServletResponse) {
+        response.sendError(400, "Parameter is empty")
+    }
+
+    private fun sendErrorParameterIsNotInteger(response: HttpServletResponse) {
+        response.sendError(400, "Parameter is not integer")
+    }
+
+    private fun sendErrorNotFoundUserWithId(response: HttpServletResponse) {
+        response.sendError(404)
+    }
+
+    private fun allUsersToJson(): String {
+        return gson.toJson(userDAO.requestAllUsers())
+    }
+
+    private fun useToJson(user: User): String {
+        return gson.toJson(user)
     }
 }
