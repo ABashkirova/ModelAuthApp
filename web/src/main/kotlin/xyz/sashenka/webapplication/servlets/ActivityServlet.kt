04ee777cf7a2d0708fe62331696dd63ff28366eb
ABashkirova <alex.bashkirova@gmail.com>
2020-05-03 15:40:52 +0700

    Refactor ActivityServlet with common code in HandleError

--- web/src/main/kotlin/xyz/sashenka/webapplication/servlets/ActivityServlet.kt
+++ web/src/main/kotlin/xyz/sashenka/webapplication/servlets/ActivityServlet.kt
@@ -5,7 +5,9 @@ import com.google.inject.Inject
 import com.google.inject.Singleton
 import org.apache.logging.log4j.kotlin.KotlinLogger
 import xyz.sashenka.modelauthapp.dao.SessionDAO
+import xyz.sashenka.modelauthapp.model.domain.UserSession
 import xyz.sashenka.webapplication.di.logger.InjectLogger
+import xyz.sashenka.webapplication.servlets.HandleError.Companion.sendErrorNotFound
 import java.io.IOException
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServlet
@@ -16,35 +18,65 @@ import javax.servlet.http.HttpServletResponse
 class ActivityServlet : HttpServlet() {
     @Inject
     lateinit var gson: Gson
+
     @Inject
     lateinit var sessionDAO: SessionDAO
+
     @InjectLogger
     lateinit var logger: KotlinLogger
+
+    private val ID = "id"
+    private val ACCESS_ID = "accessId"
 
     @Throws(ServletException::class, IOException::class)
     override fun service(request: HttpServletRequest, response: HttpServletResponse) {
 
         val query = request.queryString
         when {
-            query.isNullOrEmpty() -> {
-                val json = gson.toJson(sessionDAO.selectAll())
-                response.writer.write(json)
-            }
-            query.contains("id") -> try {
-                val id = request.getParameter("id").toInt()
-                val json = gson.toJson(sessionDAO.selectById(id))
-                response.writer.write(json)
-            } catch (e: NumberFormatException) {
-                response.sendError(400, e.message)
-            }
-            query.contains("accessId") -> try {
-                val accessId = request.getParameter("accessId").toInt()
-                val json = gson.toJson(sessionDAO.selectByAccessId(accessId))
-                response.writer.write(json)
-            } catch (e: NumberFormatException) {
-                response.sendError(400, e.message)
-            }
-            else -> response.sendError(404)
+            query.isNullOrEmpty() -> response.writer.write(allSessionToJson())
+            query.contains(ID) -> handleRequestWithIdParameter(request, response)
+            query.contains(ACCESS_ID) -> handleRequestWithAccessIdParameter(request, response)
+            else -> sendErrorNotFound(response)
         }
+    }
+
+    private fun handleRequestWithAccessIdParameter(request: HttpServletRequest, response: HttpServletResponse) {
+        val idParameter = request.getParameter(ACCESS_ID)
+        if (!HandleError.sendErrorForIntegerParameterIfIsNeeded(idParameter, response)) {
+            writeActivityResponseWithAccessId(idParameter.toInt(), response)
+        }
+    }
+
+    private fun writeActivityResponseWithAccessId(accessId: Int, response: HttpServletResponse) {
+        val activitySession = sessionDAO.selectByAccessId(accessId)
+        if (activitySession == null) {
+            sendErrorNotFound(response)
+        } else {
+            response.writer.write(sessionToJson(activitySession))
+        }
+    }
+
+    private fun handleRequestWithIdParameter(request: HttpServletRequest, response: HttpServletResponse) {
+        val idParameter = request.getParameter(ID)
+        if (!HandleError.sendErrorForIntegerParameterIfIsNeeded(idParameter, response)) {
+            writeActivityResponse(idParameter.toInt(), response)
+        }
+    }
+
+    private fun writeActivityResponse(activityId: Int, response: HttpServletResponse) {
+        val activitySession = sessionDAO.selectById(activityId)
+        if (activitySession == null) {
+            sendErrorNotFound(response)
+        } else {
+            response.writer.write(sessionToJson(activitySession))
+        }
+    }
+
+    private fun sessionToJson(session: UserSession): String {
+        return gson.toJson(session)
+    }
+
+    private fun allSessionToJson(): String {
+        return gson.toJson(sessionDAO.selectAll())
     }
 }
