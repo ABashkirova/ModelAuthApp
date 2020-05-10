335e36d06201b127fae0b79225bb22aacb90575b
ABashkirova <alex.bashkirova@gmail.com>
2020-05-10 23:36:05 +0700

    Move gson dependency to root gradle

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
     compile("javax.servlet:javax.servlet-api:3.1.0")
 
     implementation("com.google.inject.extensions:guice-servlet:4.2.3")
-    implementation("com.google.code.gson:gson:2.8.6")
     compile(project(":app"))
 }
 
