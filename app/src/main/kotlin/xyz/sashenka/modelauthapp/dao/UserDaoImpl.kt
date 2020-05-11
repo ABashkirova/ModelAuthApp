6233296132c5586f6931b5212e045438414c4096
ABashkirova <alex.bashkirova@gmail.com>
2020-05-11 14:51:15 +0700

    Fix error in fun getAll

--- app/src/main/kotlin/xyz/sashenka/modelauthapp/dao/UserDaoImpl.kt
+++ app/src/main/kotlin/xyz/sashenka/modelauthapp/dao/UserDaoImpl.kt
@@ -3,6 +3,7 @@ package xyz.sashenka.modelauthapp.dao
 import com.google.inject.Inject
 import xyz.sashenka.modelauthapp.model.dto.db.DBUser
 import javax.persistence.EntityManager
+import javax.persistence.TypedQuery
 import javax.persistence.criteria.CriteriaQuery
 import javax.persistence.criteria.Root
 
@@ -10,25 +11,27 @@ class UserDaoImpl @Inject constructor(
     var entityManager: EntityManager
 ) : UserDao {
     override fun save(user: DBUser) {
-        entityManager.get().merge(user)
+        entityManager.merge(user)
     }
 
     override fun getAll(): List<DBUser> {
-        val createQuery: CriteriaQuery<DBUser> =
-            entityManager.get().criteriaBuilder.createQuery(DBUser::class.java)
-        return entityManager.get().createQuery(createQuery).resultList
+        val criteriaQuery = entityManager.criteriaBuilder.createQuery(DBUser::class.java)
+        val rootEntry: Root<DBUser> = criteriaQuery.from(DBUser::class.java)
+        val all: CriteriaQuery<DBUser> = criteriaQuery.select(rootEntry)
+        val allQuery: TypedQuery<DBUser> = entityManager.createQuery(all)
+        return allQuery.resultList
     }
 
     override fun findUserById(id: Int): DBUser? {
-        return entityManager.get().find(DBUser::class.java, id)
+        return entityManager.find(DBUser::class.java, id)
     }
 
     override fun findUser(login: String): DBUser? {
         val createQuery: CriteriaQuery<DBUser> =
-            entityManager.get().criteriaBuilder.createQuery(DBUser::class.java)
+            entityManager.criteriaBuilder.createQuery(DBUser::class.java)
         val root: Root<DBUser> = createQuery.from(DBUser::class.java)
-        createQuery.where(root.get<Any>("name").`in`(login))
-        return entityManager.get().createQuery(createQuery).singleResult
+        createQuery.where(root.get<Any>("login").`in`(login))
+        return entityManager.createQuery(createQuery).singleResult
     }
 }
 
