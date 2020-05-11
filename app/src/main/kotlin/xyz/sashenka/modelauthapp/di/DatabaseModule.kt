de8b989a04470dbab88432095820d156a6e3640c
ABashkirova <alex.bashkirova@gmail.com>
2020-05-11 14:57:56 +0700

    Add persist injection

--- app/src/main/kotlin/xyz/sashenka/modelauthapp/di/DatabaseModule.kt
+++ app/src/main/kotlin/xyz/sashenka/modelauthapp/di/DatabaseModule.kt
@@ -1,32 +1,17 @@
 package xyz.sashenka.modelauthapp.di
 
 import com.google.inject.AbstractModule
-import com.google.inject.Inject
-import com.google.inject.Provides
 import com.google.inject.Singleton
 import com.google.inject.persist.jpa.JpaPersistModule
-import xyz.sashenka.modelauthapp.dao.ResourceDAO
-import xyz.sashenka.modelauthapp.dao.SessionDAO
-import xyz.sashenka.modelauthapp.dao.UserDao
-import xyz.sashenka.modelauthapp.dao.UserDaoImpl
-import xyz.sashenka.modelauthapp.service.DBService
+import xyz.sashenka.modelauthapp.dao.*
 
 class DatabaseModule : AbstractModule() {
     override fun configure() {
         super.configure()
         install(JpaPersistModule("AaaPersistenceUnit"))
+        bind(PersistenceInitializer::class.java).asEagerSingleton()
         bind(UserDao::class.java).to(UserDaoImpl::class.java).`in`(Singleton::class.java)
-
-        bind(DBService::class.java).toProvider(DatabaseProvider::class.java).`in`(Singleton::class.java)
+        bind(ResourceDao::class.java).to(ResourceDaoImpl::class.java).`in`(Singleton::class.java)
+        bind(SessionDao::class.java).to(SessionDaoImpl::class.java).`in`(Singleton::class.java)
     }
-
-    @Inject
-    @Provides
-    @Singleton
-    fun createResourceDao(dbService: DBService): ResourceDAO = ResourceDAO(dbService)
-
-    @Inject
-    @Provides
-    @Singleton
-    fun createSessionDao(dbService: DBService): SessionDAO = SessionDAO(dbService)
 }
