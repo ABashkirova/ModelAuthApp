8a78d66b5175c693a2923eb63d6eeff598701c93
kozlykov <kozlyakov.m@gmail.com>
2020-05-15 22:38:10 +0700

    Change env vars names

--- app/src/main/kotlin/xyz/sashenka/modelauthapp/di/DatabaseModule.kt
+++ app/src/main/kotlin/xyz/sashenka/modelauthapp/di/DatabaseModule.kt
@@ -9,19 +9,19 @@ import java.util.HashMap
 
 class DatabaseModule : AbstractModule() {
     val persistenceUnitName = "AaaPersistenceUnit"
-    private val envDriver: String = System.getenv("DBDRIVER") ?: "org.h2.Driver"
-    private val envUrl: String = System.getenv("DBURL") ?: "jdbc:h2:file:./AAA"
-    private val envLogin: String = System.getenv("DBLOGIN") ?: "sa"
-    private val envPass: String = System.getenv("DBPASS") ?: ""
+    private val envDriver: String = System.getenv("JDBC_DATABASE_DRIVER") ?: "org.h2.Driver"
+    private val envUrl: String = System.getenv("JDBC_DATABASE_URL") ?: "jdbc:h2:file:./AAA"
+    private val envLogin: String = System.getenv("JDBC_DATABASE_USERNAME") ?: "sa"
+    private val envPass: String = System.getenv("JDBC_DATABASE_PASSWORD") ?: ""
 
     override fun configure() {
         super.configure()
-        /*
+
          val jpaModule = JpaPersistModule(persistenceUnitName)
         jpaModule.properties(getDatabaseEnvironments())
         install(jpaModule)
-         */
-        install(JpaPersistModule("AaaPersistenceUnit"))
+
+//        install(JpaPersistModule("AaaPersistenceUnit"))
         bind(PersistenceInitializer::class.java).asEagerSingleton()
         bind(UserDao::class.java).to(UserDaoImpl::class.java).`in`(Singleton::class.java)
         bind(ResourceDao::class.java).to(ResourceDaoImpl::class.java).`in`(Singleton::class.java)
