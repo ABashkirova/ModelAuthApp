10b15920d69e8b371512b6870edc1260d5c0b6ea
kozlykov <kozlyakov.m@gmail.com>
2020-05-19 12:24:35 +0700

    Add ajax query to Accesses class

--- web/src/main/webapp/react/accesses.js
+++ web/src/main/webapp/react/accesses.js
@@ -1,9 +1,44 @@
 class Accesses extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        accesses: []
+      };
+  }
+
+
+  componentDidMount() {
+    const url = 'ajax/authority?userId='+this.props.userId
+
+    fetch(url)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          accesses: result
+        })
+      })
+    }
+
     render() {
+        const { accesses } = this.state
+
+        const result = accesses.map(access => {
+            return (
+                //событие onclick обрабатывает родительский метод, который передается через props
+                <tr onClick={() => {this.props.handleClick('Accesses', access.id)}} key={access.id}>
+                    <td>{access.id}</td>
+                    <td>{access.userId}</td>
+                    <td>{access.resource}</td>
+                    <td>{access.role}</td>
+                </tr>
+            )
+        })
         return (
-        <div>
-            <button className="btn btn-light" onClick={() => {this.props.handleClick('Users')}}>Назад</button>
-            <p>id: {this.props.userId}</p>
+        <div className="container">
+            <button className="btn btn-outline-primary" onClick={() => {this.props.handleClick('Users')}}>Назад</button>
+            <table className="table">
+                {result}
+            </table>
         </div>
         )
     }
