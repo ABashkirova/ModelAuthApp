9ad5bb354ff1b1f9d83a2e1f6c5367bafee240e2
ABashkirova <alex.bashkirova@gmail.com>
2020-05-21 09:06:10 +0700

    Add implementation for Activities component

--- web/src/main/webapp/react/activities.js
+++ web/src/main/webapp/react/activities.js
@@ -1,5 +1,56 @@
 class Activities extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activities: []
+        };
+    }
+
+    componentDidMount() {
+        const url = 'ajax/activity?authorityId=' + this.props.accessId
+
+        fetch(url)
+            .then(result => result.json())
+            .then(result => {
+                this.setState({
+                    activities: result
+                })
+            })
+    }
+
     render() {
-        return <p>Activities</p>
+        const {activities} = this.state
+
+        const result = activities.map(activity => {
+            return (
+                <tbody>
+                <tr>
+                    <td>{activity.id}</td>
+                    <td>{activity.dateStart}</td>
+                    <td>{activity.dateEnd}</td>
+                    <td>{activity.volume}</td>
+                </tr>
+                </tbody>
+            )
+        })
+        return (
+            <div className="container">
+                <button className="btn btn-outline-primary" onClick={() => {
+                    this.props.handleClick('Accesses')
+                }}>Назад
+                </button>
+                <table className="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">dateStart</th>
+                        <th scope="col">dateEnd</th>
+                        <th scope="col">volume</th>
+                    </tr>
+                    </thead>
+                    {result}
+                </table>
+            </div>
+        )
     }
 }
