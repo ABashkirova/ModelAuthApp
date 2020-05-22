472c889da954f87346b530b6f814790d657fb284
ABashkirova <alex.bashkirova@gmail.com>
2020-05-22 15:57:21 +0700

    Update selects

--- app/src/main/kotlin/xyz/sashenka/modelauthapp/repository/SessionRepositoryImpl.kt
+++ app/src/main/kotlin/xyz/sashenka/modelauthapp/repository/SessionRepositoryImpl.kt
@@ -5,6 +5,8 @@ import com.google.inject.Singleton
 import xyz.sashenka.modelauthapp.dao.SessionDao
 import xyz.sashenka.modelauthapp.model.domain.UserSession
 import xyz.sashenka.modelauthapp.model.dto.db.DBAccess
+import xyz.sashenka.modelauthapp.model.dto.db.DBUserSession
+import java.sql.Date
 
 @Singleton
 class SessionRepositoryImpl : SessionRepository {
@@ -12,6 +14,12 @@ class SessionRepositoryImpl : SessionRepository {
     lateinit var sessionDao: SessionDao
 
     override fun addSession(access: DBAccess, session: UserSession) {
-        sessionDao.save(access, session)
+        val dbSession = DBUserSession()
+        dbSession.access = access
+        dbSession.dateStart = Date.valueOf(session.dateStart)
+        dbSession.dateEnd = Date.valueOf(session.dateEnd)
+        dbSession.volume = session.volume
+
+        sessionDao.save(dbSession)
     }
 }
