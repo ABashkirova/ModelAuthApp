5658a157bb5998485807639a79d4a3902d9fa035
ABashkirova <alex.bashkirova@gmail.com>
2020-05-22 09:55:41 +0700

    Add logger

--- app/src/main/kotlin/xyz/sashenka/modelauthapp/di/DatabaseModule.kt
+++ app/src/main/kotlin/xyz/sashenka/modelauthapp/di/DatabaseModule.kt
@@ -1,13 +1,14 @@
 package xyz.sashenka.modelauthapp.di
 
 import com.google.inject.AbstractModule
-import com.google.inject.Singleton
 import com.google.inject.persist.jpa.JpaPersistModule
+import org.apache.logging.log4j.kotlin.KotlinLogger
+import org.apache.logging.log4j.kotlin.logger
 import org.flywaydb.core.Flyway
-import xyz.sashenka.modelauthapp.dao.*
 import java.util.*
 
 class DatabaseModule : AbstractModule() {
+    private val logger: KotlinLogger = logger()
     private val persistenceUnitName = "AaaPersistenceUnit"
     private var envDriver: String = ""
     private var envUrl: String = ""
@@ -23,13 +24,13 @@ class DatabaseModule : AbstractModule() {
         jpaModule.properties(getDatabaseEnvironments())
         install(jpaModule)
         bind(PersistenceInitializer::class.java).asEagerSingleton()
-        bind(UserDao::class.java).to(UserDaoImpl::class.java).`in`(Singleton::class.java)
-        bind(ResourceDao::class.java).to(ResourceDaoImpl::class.java).`in`(Singleton::class.java)
-        bind(SessionDao::class.java).to(SessionDaoImpl::class.java).`in`(Singleton::class.java)
         migrate()
     }
 
     private fun getDatabaseEnvironments(): MutableMap<String, Any> {
+        logger.info {
+            "Env to map"
+        }
         val envConfig: MutableMap<String, Any> = HashMap()
         envConfig["javax.persistence.jdbc.url"] = envUrl
         envConfig["javax.persistence.jdbc.user"] = envLogin
@@ -41,15 +42,17 @@ class DatabaseModule : AbstractModule() {
 
     private fun setEnv() {
         val url = System.getenv("DATABASE_URL")
+        logger.info { "Env: [DATABASE_URL, $url]" }
         if (url.isNullOrEmpty()) {
-            print("HELLO FROM SET ENV isNullOrEmpty")
+            logger.info { "url.isNullOrEmpty(), use default local" }
+            // TODO: переписать на Property
             envUrl = "jdbc:h2:file:./AAA"
             envLogin = "sa"
             envPass = ""
             envDriver = "org.h2.Driver"
             hibernateDialect = "org.hibernate.dialect.H2Dialect"
         } else {
-            print("HELLO FROM SET ENV $url")
+            logger.info { "setEnv from url" }
             envUrl = System.getenv("JDBC_DATABASE_URL")
             envLogin = System.getenv("JDBC_DATABASE_USERNAME")
             envPass = System.getenv("JDBC_DATABASE_PASSWORD")
@@ -59,6 +62,7 @@ class DatabaseModule : AbstractModule() {
     }
 
     private fun migrate() {
+        logger.info { "Flyway migration" }
         Flyway
             .configure()
             .dataSource(envUrl, envLogin, envPass)
