a0af2251d3c0a3936b0d61a522a7d09cb3b8bbe5
ABashkirova <alex.bashkirova@gmail.com>
2020-05-22 15:54:54 +0700

    Add DBAccess.hbm.xml hibernate xml description

--- app/src/main/kotlin/xyz/sashenka/modelauthapp/model/dto/db/DBAccess.kt
+++ app/src/main/kotlin/xyz/sashenka/modelauthapp/model/dto/db/DBAccess.kt
@@ -1,23 +1,11 @@
 package xyz.sashenka.modelauthapp.model.dto.db
 
 import com.google.gson.annotations.Expose
-import javax.persistence.*
 
-@Entity
-@Table(name = "accesses")
-data class DBAccess @JvmOverloads constructor(
-    @Expose @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id")
-    val id: Int = 0,
-
-    @Expose @Column(name = "user_id")
-    val userId: Int,
-
-    @Expose @Column(name = "resource")
-    val resource: String,
-
-    @Expose @Column(name = "role")
-    val role: String,
-
-    @Version
-    val version: Long = 1
-)
+class DBAccess {
+    @Expose var id: Int = 1
+    @Expose var resource: String = ""
+    @Expose var role: String = ""
+    var user: DBUser? = null
+    var version: Long = 1
+}
